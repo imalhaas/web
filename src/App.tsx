22d552a9fbf53b9ad7a739fc64b585e4105ae11d
Lucas Magalhaes <lucasdecassia9@hotmail.com>
2024-09-15 16:16:11 -0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
+import { useEffect, useState } from 'react'
 
-import './App.css'
+import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
-import { Dialog } from './components/ui/dialog'
-// import { EmptyGoals } from './components/empty-goals'
-
-
-
+import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 seconds
+  })
 
   return (
-     
     <Dialog>
-      {/* <EmptyGoals />*/}
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
-      <Summary />
       <CreateGoal />
     </Dialog>
   )
 }
-
