227a7707491510528933351bfd51ee5a6211f1b9
Lucas Magalhaes <lucasdecassia9@hotmail.com>
2024-09-15 01:34:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+import { Dialog } from './components/ui/dialog'
+// import { EmptyGoals } from './components/empty-goals'
+
+
+
+
+export function App() {
+
+  return (
+     
+    <Dialog>
+      {/* <EmptyGoals />*/}
+
+      <Summary />
+      <CreateGoal />
+    </Dialog>
+  )
+}
+
